5ea0e2f539f9b73693b41a9d4000fe997017b1b5
nuniromerook <juancnunz.contacto@gmail.com>
2026-09-01 08:07:58 -0300

    feat: create Sidebar component with navigation, store management, and password reset functionality

--- frontend-admin/src/components/Sidebar.jsx
+++ frontend-admin/src/components/Sidebar.jsx
@@ -100,7 +100,7 @@ const Sidebar = () => {
             </li>
 
             <li>
-              <details className="group [&_summary::-webkit-details-marker]:hidden">
+              <details open className="group [&_summary::-webkit-details-marker]:hidden">
                 <summary className="flex items-center justify-between rounded-lg px-4 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-100 hover:text-gray-900">
                   <p className="flex items-center gap-x-2 font-semibold">
                     <Store className={iconStyle} />
